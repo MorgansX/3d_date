bc07ad713eeb17d632a8e7da51a1d91599bf8718
arthurmorgan <echozilladev@outlook.com>
2025-07-29 18:58:00 +0300

    fixed build

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import * as THREE from 'three';
-import {SIZES} from "./constants/dimensions.ts";
+import { SIZES } from "./constants/dimensions.ts";
 import Renderer from "./core/Renderer.ts";
 import Scene from "./core/Scene.ts";
 import moment from "moment";
 
-import {FontLoader} from "three/examples/jsm/loaders/FontLoader";
-import {TextGeometry} from "three/examples/jsm/geometries/TextGeometry";
+import { FontLoader, Font } from "three/addons/loaders/FontLoader.js";
+import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
 
 import './style.css';
 
 const canvas = document.querySelector('canvas.app') as HTMLCanvasElement;
 const rendererInstance = Renderer.getInstance(canvas);
-const renderer= rendererInstance.getRenderer();
+const renderer = rendererInstance.getRenderer();
 const fontLoader = new FontLoader();
 
 const mouse = {
@@ -25,7 +25,8 @@ let textMesh: THREE.Mesh | null = null;
 
 const date = moment().format('D MMMM');
 
-fontLoader.load('/fonts/rubic_black.json', (font) => {
+// Added proper type annotation for font parameter
+fontLoader.load('/fonts/rubic_black.json', (font: Font) => {
     const textGeometry = new TextGeometry(
         date,
         {
@@ -50,7 +51,7 @@ fontLoader.load('/fonts/rubic_black.json', (font) => {
     renderer.render(scene.getScene(), camera);
 })
 
-const camera = new THREE.PerspectiveCamera(75, SIZES.WIDTH/ SIZES.HEIGHT, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, SIZES.WIDTH / SIZES.HEIGHT, 0.1, 1000)
 camera.position.z = 5;
 scene.add(camera)
 
